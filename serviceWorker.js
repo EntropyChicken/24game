1cac85b6b027228056a039eb5ee8038cf90d1263
EntropyChicken <jasondeng333@gmail.com>
2025-07-24 22:24:47 -0400

    Service worker for offline app

---
+++ serviceWorker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "24game-cache-v1";
+const CACHE_FILES = [
+	"./index.html",
+	"./style.css",
+	"./manifest.json",
+	"./assets/icon.png",
+	"./assets/icon-550.png",
+	"./assets/favicon.png",
+
+	// js
+	"./sketch.js",
+	"./complex.js",
+	"./operation.js",
+	"./bubble.js",
+	"./bubbleBox.js",
+	"./button.js",
+	"./duel.js",
+	"./level.js",
+	"./titleScreen.js",
+	"./serviceWorker.js",
+
+	// libraries
+	"./libraries/p5.min.js",
+	"./libraries/p5.sound.min.js",
+
+	// level sets
+	"./levelData/classicLevelsCooked.json",
+	"./levelData/classicLevelsEasy.json",
+	"./levelData/classicLevelsHard.json",
+	"./levelData/classicLevelsMedium.json",
+	"./levelData/classicLevelsTricky.json",
+	"./levelData/puzzleLevelsCrazyHard.json",
+	"./levelData/puzzleLevelsInteresting.json",
+	"./levelData/puzzleLevelsJavascript.json",
+	"./levelData/puzzleLevelsSimple.json",
+	"./levelData/scrapped.json",
+];
+
+
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			return cache.addAll(CACHE_FILES);
+		})
+	);
+});
+
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			return response || fetch(event.request);
+		})
+	);
+});

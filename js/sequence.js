cd5fe295b9d6cf43324fb53515903893e2f04a60
EntropyChicken <jasondeng333@gmail.com>
2026-07-07 23:14:17 -0400

    New puzzles

--- js/sequence.js
+++ js/sequence.js
@@ -384,8 +384,8 @@ function stringToNum(s){
 
 // evaluates all expressions for all classics and puzzles except for js puzzles, sees if they're 24
 function testAllSolutions() {
-    // let problemSets = puzzleSets.slice(0, 3).concat(puzzleSets.slice(4), classicSets);
-    for(let problemSet of puzzleSets.slice(0,3).concat(puzzleSets.slice(4))){
+    // let problemSets = puzzleSets.slice(0, 2).concat(puzzleSets.slice(3), classicSets);
+    for(let problemSet of puzzleSets.slice(0,2).concat(puzzleSets.slice(3))){
         testSolutionsOfProblemSet(problemSet,false);
     }
     for(let problemSet of classicSets){
